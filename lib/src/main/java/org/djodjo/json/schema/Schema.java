39eabceb3923467235549da5817331b327bd84f1
Kalin Maldzhanski <djodjo@djodjo.org>
2014-03-30 23:53:59 +0200

    add schema merge

--- lib/src/main/java/org/djodjo/json/schema/Schema.java
+++ lib/src/main/java/org/djodjo/json/schema/Schema.java
@@ -17,6 +17,7 @@ public abstract class Schema extends JsonObjectWrapper {
 
     public static final String TYPE_OBJECT = "object";
     public static final String TYPE_ARRAY = "array";
+    public static final String TYPE_ENUM = "enum";
     public static final String TYPE_STRING = "string";
     public static final String TYPE_NUMBER = "number";
     public static final String TYPE_BOOLEAN = "boolean";
@@ -206,5 +207,10 @@ public abstract class Schema extends JsonObjectWrapper {
         return (Schema)getEmptySchema().wrap(getJson().optJsonObject("not"));
     }
 
+
+    //TODO
+    public Schema merge(Schema schema) {
+        return this;
+    }
 
 }
